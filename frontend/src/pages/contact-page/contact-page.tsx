8d80ed7515bd0d2b0c72497d2db370d221efa0d4
Churina Margaery <elshanskaya.r@inbox.ru>
2025-05-15 20:14:26 +0300

    contact page complete

--- frontend/src/pages/contact-page/contact-page.tsx
+++ frontend/src/pages/contact-page/contact-page.tsx
@@ -1,11 +1,12 @@
 import { type JSX } from 'react'
 import Header from '../../components/header/header'
 import Footer from '../../components/footer/footer'
-
+import { ContactForm } from '../../components/contact-form/contact-form'
 export default function ContactPage(): JSX.Element {
   return (
     <>
       <Header />
+      <ContactForm />
       <Footer />
     </>
   )
